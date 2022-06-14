a0ab49d6398a69f6f09a5a457eb6603622ab3e1f
Foxegens <kopva2000@gmail.com>
2022-06-14 18:01:30 +0300

    Initial commit

---
+++ src/components/contact.jsx
@@ -0,0 +1,161 @@
+import { useState } from 'react'
+import emailjs from 'emailjs-com'
+
+const initialState = {
+  name: '',
+  email: '',
+  message: '',
+}
+export const Contact = (props) => {
+  const [{ name, email, message }, setState] = useState(initialState)
+
+  const handleChange = (e) => {
+    const { name, value } = e.target
+    setState((prevState) => ({ ...prevState, [name]: value }))
+  }
+  const clearState = () => setState({ ...initialState })
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log(name, email, message)
+    emailjs
+      .sendForm(
+        'YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', e.target, 'YOUR_USER_ID'
+      )
+      .then(
+        (result) => {
+          console.log(result.text)
+          clearState()
+        },
+        (error) => {
+          console.log(error.text)
+        }
+      )
+  }
+  return (
+    <div>
+      <div id='contact'>
+        <div className='container'>
+          <div className='col-md-8'>
+            <div className='row'>
+              <div className='section-title'>
+                <h2>Свяжитесь с нами</h2>
+                <p>
+                  Пожалуйста заполните форму ниже чтобы отправить нам свой запрос, и мы ответим вам в ближайшее время.
+                </p>
+              </div>
+              <form name='sentMessage' validate onSubmit={handleSubmit}>
+                <div className='row'>
+                  <div className='col-md-6'>
+                    <div className='form-group'>
+                      <input
+                        type='text'
+                        id='name'
+                        name='name'
+                        className='form-control'
+                        placeholder='ФИО'
+                        required
+                        onChange={handleChange}
+                      />
+                      <p className='help-block text-danger'></p>
+                    </div>
+                  </div>
+                  <div className='col-md-6'>
+                    <div className='form-group'>
+                      <input
+                        type='email'
+                        id='email'
+                        name='email'
+                        className='form-control'
+                        placeholder='Email'
+                        required
+                        onChange={handleChange}
+                      />
+                      <p className='help-block text-danger'></p>
+                    </div>
+                  </div>
+                </div>
+                <div className='form-group'>
+                  <textarea
+                    name='message'
+                    id='message'
+                    className='form-control'
+                    rows='4'
+                    placeholder='Ваше сообщение'
+                    required
+                    onChange={handleChange}
+                  ></textarea>
+                  <p className='help-block text-danger'></p>
+                </div>
+                <div id='success'></div>
+                <button type='submit' className='btn btn-custom btn-lg'>
+                  Отправить сообщение
+                </button>
+              </form>
+            </div>
+          </div>
+          <div className='col-md-3 col-md-offset-1 contact-info'>
+            <div className='contact-item'>
+              <h3>Контактная информация</h3>
+              <p>
+                <span>
+                  <i className='fa fa-map-marker'></i> Адрес
+                </span>
+                ул. Василия, Беларусь
+              </p>
+            </div>
+            <div className='contact-item'>
+              <p>
+                <span>
+                  <i className='fa fa-phone'></i> Телефон
+                </span>{' '}
+                +375-29-123-45-67
+              </p>
+            </div>
+            <div className='contact-item'>
+              <p>
+                <span>
+                  <i className='fa fa-envelope-o'></i> Email
+                </span>{' '}
+                example@gmail.com
+              </p>
+            </div>
+          </div>
+          <div className='col-md-12'>
+            <div className='row'>
+              <div className='social'>
+                <ul>
+                  <li>
+                    <a href={props.data ? props.data.google : '/'}>
+                      <i className='fa fa-google'></i>
+                    </a>
+                  </li>
+                  <li>
+                    <a href={props.data ? props.data.telegram : '/'}>
+                      <i className='fa fa-paper-plane'></i>
+                    </a>
+                  </li>
+                  <li>
+                    <a href={props.data ? props.data.instagram : '/'}>
+                      <i className='fa fa-instagram'></i>
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div id='footer'>
+        <div className='container text-center'>
+          <p>
+            &copy; 2022 Valentin Kopatskevich React Land Page. Design by{' '}
+            <a href='' rel='nofollow'>
+              Foxegen
+            </a>
+          </p>
+        </div>
+      </div>
+    </div>
+  )
+}
---
+++ src/components/services.jsx
@@ -0,0 +1,33 @@
+export const Services = (props) => {
+  return (
+    <div id='services' className='text-center'>
+      <div className='container'>
+        <div className='section-title'>
+          <h2>Наши Услуги</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit duis sed
+            dapibus leonec.
+          </p>
+        </div>
+        <div className='row'>
+          {props.data
+            ? props.data.map((d, i) => (
+                <div key={`${d.name}-${i}`} className='col-md-4'>
+                  {' '}
+                  <img
+                  width='230px'
+                  src={d.icon}
+                  alt="logo"
+                  />
+                  <div className='service-desc'>
+                    <h3>{d.name}</h3>
+                    <p>{d.text}</p>
+                  </div>
+                </div>
+              ))
+            : 'loading'}
+        </div>
+      </div>
+    </div>
+  )
+}
