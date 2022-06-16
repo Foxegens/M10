4ae683b4d3d9b14fef3ee647377972100db461e9
Foxegens <kopva2000@gmail.com>
2022-06-16 14:16:32 +0300

    commit on 'main'

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -12,7 +12,7 @@ export const Header = (props) => {
                 </h1>
                 <p>{props.data ? props.data.paragraph : 'Loading'}</p>
                 <a
-                  href='#servises'
+                  href='#services'
                   className='btn btn-custom btn-lg page-scroll'
                 >
                   Наши услуги
